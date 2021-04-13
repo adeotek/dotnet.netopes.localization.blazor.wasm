6d1921faad8035be38e94976a4c5be0facddaca1
George Benjamin-Schonberger <george.benjamin@gmail.com>
2021-04-13 13:23:04 +0300

    v1.0.0

---
+++ src/Internal/Helpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Netopes.Localization.Blazor.Wasm.Internal
+{
+    public static class Helpers
+    {
+        public static string GetApplicationRoot()
+            => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    }
+
+    static class Extensions
+    {
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
+
+        public static IList<KeyValuePair<string, string>> Merge(this IList<KeyValuePair<string, string>> mainList, IEnumerable<KeyValuePair<string, string>> secondaryList)
+        {
+            foreach (var newItem in secondaryList)
+            {
+                if (newItem.Key == null || newItem.Value == null)
+                {
+                    continue;
+                }
+
+                var index = mainList.ToList().FindIndex(item => item.Key == newItem.Key);
+                if (index >= 0)
+                {
+                    mainList[index] = newItem;
+                }
+                else
+                {
+                    mainList.Add(newItem);
+                }
+            }
+
+            return mainList;
+        }
+    }
+}
---
+++ src/Internal/StringLocalizer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Netopes.Localization.Blazor.Wasm.Internal
+{
+    internal class StringLocalizer : IStringLocalizer
+    {
+        private IStringLocalizer _localizer;
+
+        public StringLocalizer(IStringLocalizerFactory factory)
+        {
+            _localizer = factory.Create(string.Empty, Helpers.GetApplicationRoot());
+        }
+
+        public LocalizedString this[string name] => _localizer[name];
+
+        public LocalizedString this[string name, params object[] arguments] => _localizer[name, arguments];
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures) => _localizer.GetAllStrings(includeParentCultures);
+
+        public IStringLocalizer WithCulture(CultureInfo culture)
+        {
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+            return _localizer;
+        }
+    }
+}
